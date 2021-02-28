0f3f0cb36a88882eb7605ec2fa73a4b81dceae7c
anggitaputra <anggitaputra13@gmail.com>
2021-02-28 18:48:01 +0800

    hera spa

---
+++ .vue-cli-ui/heraspa/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <Navbar />
+    <Hero />
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+import Hero from '@/components/Hero.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Navbar,
+    Hero
+  }
+  
+}
+</script>
